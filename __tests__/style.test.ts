bfaf43cef18b7827df3fcb8488dab3d7a7d465be
Henry Seed <henryseed@Henrys-Mac-mini.lan>
2020-10-08 19:30:10 +1300

    Adds tests

---
+++ __tests__/style.test.ts
@@ -0,0 +1,21 @@
+import { drawASCIIMap } from "../src/world"
+
+describe("Tests padding", () => {
+    test("1", () => {
+        const config = {
+            border: true,
+            margin: 2,
+            padding: 2
+        }
+        const map = drawASCIIMap([], config).split("\n");
+
+        console.log(map)
+
+        // checks the padding is right
+        expect(map[0][5]).toEqual(undefined)
+        expect(map[1][5]).toEqual("-")
+        expect(map[3][2]).toEqual("|")
+        expect(map[1][2]).toEqual("+")
+        
+    })
+})
